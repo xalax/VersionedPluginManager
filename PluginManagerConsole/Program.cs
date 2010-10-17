f87269694157a78b062f982feba9ca86e61fb7c8
xalax <dev@xalax.de>
2010-10-17 15:42:10 +0200

    Enhanced sample plugin console project

--- PluginManagerConsole/Program.cs
+++ PluginManagerConsole/Program.cs
@@ -22,12 +22,36 @@ namespace xalax.PluginManager.SampleConsole
     {
         static void Main(string[] args)
         {
+
+
+            #region Discover succeed due to a compatible version
+
+            Console.WriteLine("Discover all ISamplePlugin plugins with version > 1.0 and version < 1.9.3 ...");
+            
             var pm = new PluginManager(Environment.CurrentDirectory + System.IO.Path.DirectorySeparatorChar + "SamplePlugin")
-                .Register<ISamplePlugin.ISamplePlugin>(new Version(1, 0), new Version(1, 9))
+                .Register<ISamplePlugin.ISamplePlugin>(new Version(1, 0), new Version(1, 9, 3))
                     .Discover();
 
             var plugin = pm.GetPlugins<ISamplePlugin.ISamplePlugin>(p => p.GetType().Name == "SamplePlugin").FirstOrDefault();
-            Console.WriteLine(plugin.SampleMethod(1, ""));
+            Console.WriteLine("Succeeded for: " + plugin.SampleMethod(1, ""));
+            
+            #endregion
+
+            #region Discovering fail due to an incompatible version
+
+            Console.WriteLine("");
+            Console.WriteLine("Discover all ISamplePlugin plugins with version > 1.0 and version < 1.8 ...");
+
+            pm = new PluginManager(Environment.CurrentDirectory + System.IO.Path.DirectorySeparatorChar + "SamplePlugin")
+                .Register<ISamplePlugin.ISamplePlugin>(new Version(1, 0), new Version(1, 8));
+            
+            pm.OnPluginIncompatibleVersion += (sender, e) =>
+            {
+                Console.WriteLine("Failed: [{0}] is not between [{1}] and [{2}]", e.PluginVersion, e.MinVersion, e.MaxVersion);
+            };
+            pm.Discover(false); // do not throw an axception
+
+            #endregion
 
             Console.ReadKey();
         }
